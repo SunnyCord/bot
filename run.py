3062d92d4a0da7ae913024c9551662f75ed7acfa
NiceAesth <andrewcraft101@gmail.com>
2018-08-22 13:37:33 +0300

    v1.0.0U220818
    Audio is now being handled by a lavalink server
    Shortened and improved commands
    Removed verification functionality
    Removed admin call command
    Removed the need for a poll channel
    Added a feature for the bot to update itself from git
    Added support for server-wide prefixes
    Added cog loader/unloader/reloader
    Fixed avatar command

---
+++ run.py
@@ -0,0 +1,44 @@
+import discord
+from discord.ext import commands
+import sys, traceback
+import config as cfg
+from commons import mongoIO
+
+def get_config():
+    if cfg.DEBUG==True:
+        return cfg.debugConf
+    else:
+        return cfg.conf
+
+def get_prefix(bot, message):
+    """A callable Prefix for our bot. This could be edited to allow per server prefixes."""
+    prefixes = ['s.', '?', 'sunny ']
+    if message.guild:
+        guildPref = mongoIO.getSetting(message.guild.id, 'prefix')
+    if guildPref is not None:
+        prefixes.append(guildPref)
+    if not message.guild:
+        return ' '
+    return commands.when_mentioned_or(*prefixes)(bot, message)
+
+load_extensions = ['cogs.owner', 'cogs.admin', 'cogs.music', 'cogs.images', 'cogs.errorhandler', 'cogs.fun', 'cogs.settings'] 
+bot = commands.AutoShardedBot(command_prefix=get_prefix, description='Sunny Bot', pm_help=True)
+
+if __name__ == '__main__':
+    for extension in load_extensions:
+        try:
+            bot.load_extension(extension)
+        except Exception as e:
+            print(f'Failed to load extension {extension}.', file=sys.stderr)
+            traceback.print_exc()
+
+
+@bot.event
+async def on_ready():
+
+    print(f'\n\nLogged in as: {bot.user.name} - {bot.user.id}\nVersion: {discord.__version__}\n')
+
+    await bot.change_presence(activity=discord.Streaming(name='#shameless_self_promotion', type=1, url='https://www.twitch.tv/niceaesthetic'))
+    print(f'Successfully logged in and booted...!')
+
+bot.run(get_config().TOKEN, bot=True, reconnect=True)
