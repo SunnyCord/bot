15c778913289506717a8da29084800701f170a2c
NiceAesth <nice@aesth.dev>
2018-10-06 13:42:03 +0300

    Had to fix this. It was going to drive me nuts sooner or later

--- cogs/owner.py
+++ cogs/owner.py
@@ -28,7 +28,7 @@ class OwnerCog:
         repo = Repo(path=os.getcwd())
         o = repo.remotes.origin
         for fetch_info in o.pull():
-            message = message + f"\n Updated '{fetch_info.ref}' To '{fetch_info.commit}''"
+            message = message + f"\n Updated '{fetch_info.ref}' To '{fetch_info.commit}'"
         later = datetime.now()
         difference = (later - now).total_seconds()
         await ctx.send(f"Operation completed succesfully in {difference}s. Output: ```prolog\n{message}\n```")
