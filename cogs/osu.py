14e7f382b0df675255d68b105026facbd51de1ea
NiceAesth <nice@aesth.dev>
2019-04-24 23:56:05 +0300

    Added getMods back
    Will be added until osutop and compare are reworked. After that it can be removed safely.

--- cogs/osu.py
+++ cogs/osu.py
@@ -25,6 +25,35 @@ def secondsToText(secs):
     ("{0} second{1} ".format(seconds, "s" if seconds!=1 else "") if seconds else "")
     return result
 	
+def getMods(number): # TODO - REMOVE THIS AFTER REWORK
+    mod_list= []
+    if number == 0:	mod_list.append('NM')
+    if number & 1<<0:   mod_list.append('NF')
+    if number & 1<<1:   mod_list.append('EZ')
+    if number & 1<<2:   mod_list.append('TD')
+    if number & 1<<3:   mod_list.append('HD')
+    if number & 1<<4:   mod_list.append('HR')
+    if number & 1<<14:  mod_list.append('PF')
+    elif number & 1<<5:   mod_list.append('SD')
+    if number & 1<<9:   mod_list.append('NC')
+    elif number & 1<<6: mod_list.append('DT')
+    if number & 1<<7:   mod_list.append('RX')
+    if number & 1<<8:   mod_list.append('HT')
+    if number & 1<<10:  mod_list.append('FL')
+    if number & 1<<12:  mod_list.append('SO')
+    if number & 1<<15:  mod_list.append('4 KEY')
+    if number & 1<<16:  mod_list.append('5 KEY')
+    if number & 1<<17:  mod_list.append('6 KEY')
+    if number & 1<<18:  mod_list.append('7 KEY')
+    if number & 1<<19:  mod_list.append('8 KEY')
+    if number & 1<<20:  mod_list.append('FI')
+    if number & 1<<24:  mod_list.append('9 KEY')
+    if number & 1<<25:  mod_list.append('10 KEY')
+    if number & 1<<26:  mod_list.append('1 KEY')
+    if number & 1<<27:  mod_list.append('3 KEY')
+    if number & 1<<28:  mod_list.append('2 KEY')
+    return ''.join(mod_list)
+
 class osu(commands.Cog, name='osu!'):
     """osu! related commands."""
     def __init__(self,bot):
