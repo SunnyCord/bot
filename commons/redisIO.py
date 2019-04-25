354beb1b8431f5de0f67b2f78e27bdd8f46fc120
NiceAesth <nice@aesth.dev>
2019-04-25 14:05:13 +0300

    Compare rework

--- commons/redisIO.py
+++ commons/redisIO.py
@@ -1,6 +1,6 @@
 import redis
 import config as cfg
-r = redis.Redis(host='localhost', port=6379, db=0)
+r = redis.Redis(host='localhost', port=6379, db=0, decode_responses=True)
 
 def getValue(name):
     return r.get(name)
