40275a62f1431f722b4fe65b221e090b705bfb66
NiceAesth <andrewcraft101@gmail.com>
2019-05-09 17:14:57 +0300

    Codacy fixes

--- commons/checks.py
+++ commons/checks.py
@@ -1,5 +1,4 @@
 from discord.ext import commands
-import os
 from commons.mongoIO import isBlacklisted
 
 #
--- commons/redisIO.py
+++ commons/redisIO.py
@@ -1,5 +1,4 @@
 import redis
-import config as cfg
 r = redis.Redis(host='localhost', port=6379, db=0, decode_responses=True)
 
 def getValue(name):
