52f8a58b488d06774e4830e743041838befe4640
AfterWind <sergius302@gmail.com>
2019-08-27 16:23:14 +0300

    Fixed pp and accuracy if fc calculations to show them based on all hit objects. Fixed getting users from other servers. Fixed certain plays not having proper mode set.

--- commons/osu/classes/Performance.py
+++ commons/osu/classes/Performance.py
@@ -1,8 +1,9 @@
 import commons.osu.classes as osu
 
 class Performance:
-    def __init__(self, pp:float, pp_fc:float, completion:float, star_rating:float):
+    def __init__(self, pp:float, pp_fc:float, accuracy_fc:float, completion:float, star_rating:float):
         self.pp:float = pp
         self.pp_fc:float = pp_fc
+        self.accuracy_fc:float = accuracy_fc
         self.completion:float = completion
         self.star_rating:float = star_rating
