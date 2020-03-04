e0d0a10c615285deb961bafa630b9dc3ee9c4e5a
NiceAesth <andrewcraft101@gmail.com>
2020-03-04 14:40:11 +0200

    Add beatmap previews

--- listeners/osulisteners.py
+++ listeners/osulisteners.py
@@ -1,7 +1,10 @@
-import re
+import re, aiohttp, discord
+from io import BytesIO
 from discord.ext import commands
 from commons.osu import osuapiwrap
 from commons.osu import osuhelpers
+from commons.mongoIO import getOsu
+from commons.embeds.BeatmapEmbed import BeatmapEmbed
 
 class OsuListeners(commands.Cog, command_attrs=dict(hidden=True), name="osu! Chat Listener"):
     """osu! Message Listeners"""
@@ -14,7 +17,11 @@ class OsuListeners(commands.Cog, command_attrs=dict(hidden=True), name="osu! Cha
         beatmap = await osuhelpers.getBeatmapFromText(message.content)
         if beatmap is None:
             return
-        await message.channel.send(f"Found beatmap {beatmap['title']}")
+        async with aiohttp.ClientSession() as session:
+            async with session.get(f"https://b.ppy.sh/preview/{beatmap.beatmapset_id}.mp3") as resp:
+                if resp.status==200:
+                    f=BytesIO(await resp.read())
+        botReply = await message.channel.send(embed=BeatmapEmbed(beatmap), file=discord.File(f, filename="Preview.mp3"))
 
 
 def setup(bot):
