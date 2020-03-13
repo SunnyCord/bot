9d25f3f2e1a0f1550dea28f2b03138d59e9561c7
Baciu Andrei <nice@aesth.dev>
2020-03-13 19:37:30 +0200

    Reworked Sunny
    - Add config class
    - Move config to JSON file
    - Add bot class
    - Remove duplicate config imports (now inherited from bot class)
    - Properly separate commons from classes
    - Change pymongo to motor
    - Update cogs to reflect async DB change
    - Remove duplicate connections to mongoDB
    - Add preferred server setting

--- cogs/settings.py
+++ cogs/settings.py
@@ -2,16 +2,9 @@ import discord
 from discord.ext import commands
 from commons import mongoIO
 from commons import checks
-import config as cfg
 from datetime import datetime
 import time
 disableKeywords = ['none','disable','off','disabled']
-
-def get_config():
-    if cfg.DEBUG==True:
-        return cfg.debugBotConfig
-    else:
-        return cfg.conf
 
 class Settings(commands.Cog):
     """Commands user for changing server-based settings."""
@@ -24,14 +17,14 @@ class Settings(commands.Cog):
     async def prefix(self, ctx, prefix: str):
         """Sets the server-wide prefix."""
         if prefix.lower() in disableKeywords:
-            mongoIO.setPrefix(ctx.message.guild, None)
+            await self.bot.mongoIO.setPrefix(ctx.message.guild, None)
             prefix="turned off"
         else:
             if len(prefix) > 6:
                 await ctx.send("Prefix longer than 6 characters. Shortening")
                 prefix = prefix[:6]
-            mongoIO.setPrefix(ctx.message.guild, prefix)
-        embed=discord.Embed(title="Setting updated", description=f'Server-wide prefix is now {prefix}', color=get_config().COLOR, timestamp=datetime.utcnow())
+            await self.bot.mongoIO.setPrefix(ctx.message.guild, prefix)
+        embed=discord.Embed(title="Setting updated", description=f'Server-wide prefix is now {prefix}', color=self.bot.config.color, timestamp=datetime.utcnow())
         embed.set_author(name=ctx.message.author.name, icon_url=ctx.message.author.avatar_url)
         embed.set_thumbnail(url="https://i.imgur.com/ubhUTNH.gif")
         embed.set_footer(text=self.bot.user.name, icon_url=self.bot.user.avatar_url)
@@ -42,40 +35,40 @@ class Settings(commands.Cog):
     async def rebuild(self, ctx, *, args = "normal"):
         """Rebuilds the database. (Owner-only)"""
         start_time = time.time()
-        mongoIO.wipe()
+        await self.bot.mongoIO.wipe()
         servers = list(self.bot.guilds)
         args = args.lower()
         for x in range(len(servers)):
             if args == "debug":
                 print (f'Adding server {servers[x-1].name}')
-            mongoIO.addServer(servers[x-1])
+            await self.bot.mongoIO.addServer(servers[x-1])
             for member in servers[x-1].members:
-                if not member.bot and not mongoIO.userExists(member):
+                if not member.bot and not await self.bot.mongoIO.userExists(member):
                     if args == "debug":
                         print (f'Adding member {member.name}')
-                    mongoIO.addUser(member)
+                    await self.bot.mongoIO.addUser(member)
         await ctx.send(f'Done rebuilding. {time.time() - start_time}s')
 
     @commands.is_owner()
     @commands.command(hidden=True)
     async def blacklist(self, ctx, user: discord.Member):
         """Blacklists a user from the bot. (Owner-only)"""
-        if not mongoIO.userExists(user):
-            mongoIO.addUser(user, True)
+        if not await self.bot.mongoIO.userExists(user):
+            await self.bot.mongoIO.addUser(user, True)
             await ctx.send("User blacklisted.")
         else:
-            mongoIO.blacklistUser(user)
+            await self.bot.mongoIO.blacklistUser(user)
             await ctx.send("User blacklisted.")
 
     @commands.is_owner()
     @commands.command(hidden=True)
     async def unblacklist(self, ctx, user: discord.Member):
         """Unblacklists a user from the bot. (Owner-only)"""
-        mongoIO.unblacklistUser(user)
+        await self.bot.mongoIO.unblacklistUser(user)
         await ctx.send("User unblacklisted.")
 
     async def on_guild_join(self, guild):
-        mongoIO.addServer(guild)
+        await self.bot.mongoIO.addServer(guild)
 
 def setup(bot):
     bot.add_cog(Settings(bot))
