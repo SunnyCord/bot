f4db4c64115f1b393e00d73bb6d89ff65071d00b
SushiPython <57518856+SushiPython@users.noreply.github.com>
2020-10-16 01:15:53 -0600

    [cogs:information] Embed optimization (#32)
    * Code optimization
    
    Iterates over a dict instead of manually adding each embed field.
    
    * Re-implement formatting on serverinfo command embed
    
    Co-authored-by: NiceAesth <andrewcraft101@gmail.com>

--- cogs/information.py
+++ cogs/information.py
@@ -17,15 +17,19 @@ class Information(commands.Cog):
             game = None
         voice_state = None if not user.voice else user.voice.channel
         embed = discord.Embed(timestamp=ctx.message.created_at, colour=self.bot.config.color)
-        embed.add_field(name='User ID', value=user.id, inline=True)
-        embed.add_field(name='Nick', value=user.nick, inline=True)
-        embed.add_field(name='Status', value=user.status, inline=True)
-        embed.add_field(name='On Mobile', value=user.is_on_mobile(), inline=True)
-        embed.add_field(name='In Voice', value=voice_state, inline=True)
-        embed.add_field(name='Game', value=game, inline=True)
-        embed.add_field(name='Highest Role', value=user.top_role.name, inline=True)
-        embed.add_field(name='Account Created', value=user.created_at.__format__('%A, %d. %B %Y @ %H:%M:%S'))
-        embed.add_field(name='Join Date', value=user.joined_at.__format__('%A, %d. %B %Y @ %H:%M:%S'))
+        embed_values = {
+        	"User ID": user.id,
+        	"Nick": user.nick,
+        	"Status": user.status,
+        	"On Mobile": user.is_on_mobile(),
+        	"In Voice": voice_state,
+        	"Game": game,
+        	"Highest Role": user.top_role.name,
+        	"Account Created": user.created_at.__format__('%A, %d. %B %Y @ %H:%M:%S'),
+        	"Join Date": user.joined_at.__format__('%A, %d. %B %Y @ %H:%M:%S')
+        }
+        for n, v in embed_values.items():
+        	embed.add_field(name=n, value=v, inline=True)
         embed.set_thumbnail(url=user.avatar_url)
         embed.set_author(name=user.name, icon_url=user.avatar_url)
         embed.set_footer(text=self.bot.user.name, icon_url=self.bot.user.avatar_url)
@@ -37,16 +41,20 @@ class Information(commands.Cog):
         emoji_count = len(ctx.guild.emojis)
         channel_count = len([x for x in ctx.guild.channels if isinstance(x, discord.channel.TextChannel)])
         embed = discord.Embed(color=self.bot.config.color, timestamp=ctx.message.created_at)
-        embed.add_field(name='Name (ID)', value=f"{ctx.guild.name} ({ctx.guild.id})")
-        embed.add_field(name='Owner', value=ctx.guild.owner, inline=False)
-        embed.add_field(name='Members', value=ctx.guild.member_count)
-        embed.add_field(name='Text Channels', value=str(channel_count))
-        embed.add_field(name='Region', value=ctx.guild.region)
-        embed.add_field(name='Verification Level', value=str(ctx.guild.verification_level))
-        embed.add_field(name='Highest role', value=ctx.guild.roles[-1])
-        embed.add_field(name='Number of roles', value=str(role_count))
-        embed.add_field(name='Number of emotes', value=str(emoji_count))
-        embed.add_field(name='Created At', value=ctx.guild.created_at.__format__('%A, %d. %B %Y @ %H:%M:%S'))
+        embed_values = {
+        	"Name (ID)": (f"{ctx.guild.name} ({ctx.guild.id})", False),
+        	"Owner": (ctx.guild.owner, False),
+        	"Member Count": (ctx.guild.member_count, True),
+        	"Text Channels": (str(channel_count), True),
+        	"Region": (ctx.guild.region, True),
+        	"Verification Level": (str(ctx.guild.verification_level), True),
+        	"Highest Role": (ctx.guild.roles[-1], True),
+        	"Number of Roles": (str(role_count), True),
+        	"Number of Emotes": (str(emoji_count), True),
+        	"Created On": (ctx.guild.created_at.__format__('%A, %d. %B %Y @ %H:%M:%S'), True)
+        }
+        for n, v in embed_values.items():
+        	embed.add_field(name=n, value=v[0], inline=v[1])
         embed.set_thumbnail(url=ctx.guild.icon_url)
         embed.set_author(name=ctx.author.name, icon_url=ctx.author.avatar_url)
         embed.set_footer(text=self.bot.user.name, icon_url=self.bot.user.avatar_url)
