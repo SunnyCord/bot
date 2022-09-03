840be3a2b232bffa985e7d1841fb28a8844ec930
Andrei Baciu <nice@aesth.dev>
2022-09-03 13:04:14 +0300

    Update run.py

--- run.py
+++ run.py
@@ -1,12 +1,10 @@
-import os
 import logging
-import discord
 import sentry_sdk
 from classes.bot import Sunny
 
 logger = logging.getLogger()
 bot = Sunny()
-# sentry_sdk.init(bot.config.sentry)
+sentry_sdk.init(bot.config.sentry)
 
 if __name__ == "__main__":
     bot.run(reconnect=True)
