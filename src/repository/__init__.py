324b7a49586e9d2c9430e158ecbc01612bd30987
NiceAesth <nice@aesth.dev>
2023-01-31 02:21:05 +0200

    feat: add guild settings repository

--- src/repository/__init__.py
+++ src/repository/__init__.py
@@ -2,5 +2,6 @@ from __future__ import annotations
 
 from .beatmap import BeatmapRepository
 from .osu import OsuRepository
+from .settings import SettingsRepository
 from .stats import StatsRepository
 from .user import UserRepository
--- src/service/__init__.py
+++ src/service/__init__.py
@@ -2,5 +2,6 @@ from __future__ import annotations
 
 from .beatmap import BeatmapService
 from .osu import OsuService
+from .settings import SettingsService
 from .stats import StatsService
 from .user import UserService
