3cee0f643d59fc2f5b74cdc7a343337a0237424a
NiceAesth <8437201+NiceAesth@users.noreply.github.com>
2023-03-09 21:21:53 +0200

    feat: add sentry transactions to commands (#76)
    * feat: add sentry transactions to commands
    
    * style: add context typing
    
    * style: return types

--- src/run.py
+++ src/run.py
@@ -5,6 +5,8 @@ from __future__ import annotations
 
 import sentry_sdk
 from classes.bot import Sunny
+from discord.ext.commands import Context
+from sentry_sdk.tracing import Transaction
 
 bot = Sunny()
 
@@ -13,6 +15,25 @@ sentry_sdk.init(
     environment=bot.config.environment,
     traces_sample_rate=1.0,
 )
+
+
+@bot.before_invoke
+async def before_invoke(ctx: Context) -> None:
+    transaction = Transaction(
+        name=ctx.command.qualified_name,
+        op="command",
+    )
+    transaction.set_tag("user.id", ctx.author.id)
+    transaction.set_tag("user.username", ctx.author.name)
+    transaction.set_tag("guild", ctx.guild.id)
+    transaction.set_tag("channel", ctx.channel.id)
+    ctx.transaction = sentry_sdk.start_transaction(transaction)
+
+
+@bot.after_invoke
+async def after_invoke(ctx: Context) -> None:
+    ctx.transaction.finish()
+
 
 if __name__ == "__main__":
     bot.run(reconnect=True)
