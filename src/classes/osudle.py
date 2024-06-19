5bb02e157fcb97e03d58c5f65bb82d4c6edb8da2
NiceAesth <nice@aesth.dev>
2024-06-19 21:29:23 +0300

    feat: add skip button, improve logic for osudle game

--- src/classes/osudle.py
+++ src/classes/osudle.py
@@ -9,14 +9,26 @@ from abc import abstractmethod
 from io import BytesIO
 from typing import TYPE_CHECKING
 
+import discord
 from discord import File
 
 from common import humanizer
 
 if TYPE_CHECKING:
+    from typing import Any
+
     from aiosu.models import Beatmapset
     from aiosu.models import Gamemode
     from discord import Interaction
+
+
+class OsudleSkipButton(discord.ui.Button["BaseOsudleGame"]):
+    def __init__(self, game: BaseOsudleGame) -> None:
+        super().__init__(style=discord.ButtonStyle.danger, label="Skip")
+        self.game = game
+
+    async def callback(self, _: discord.Interaction):
+        self.game.skip()
 
 
 class BaseOsudleGame(ABC):
@@ -28,27 +40,29 @@ class BaseOsudleGame(ABC):
         "current_task",
         "current_beatmapset",
         "timeout",
-        "lock",
         "scores",
+        "current_view",
+        "latest_beatmap_message",
     )
 
     def __init__(self) -> None:
         self.interaction = None
         self.running = False
-        self.lock = asyncio.Lock()
         self.stop_event = asyncio.Event()
         self.mode = None
         self.current_task = None
         self.current_beatmapset = None
         self.timeout = 60
         self.scores = {}
+        self.current_view = None
+        self.latest_beatmap_message = None
 
     def get_formatted_scoreboard(self) -> str:
         if not self.scores:
-            return "Nobody got any points. :("
+            return "\nNobody got any points. :("
 
         return "\n".join(
-            f"### {i + 1}. <@{user}> - {score}"
+            f"### {i + 1}. <@{user}> - {score} guesses"
             for i, (user, score) in enumerate(
                 sorted(self.scores.items(), key=lambda x: x[1], reverse=True)[:3],
             )
@@ -66,9 +80,11 @@ class BaseOsudleGame(ABC):
 
     async def send_response(self, *args, **kwargs) -> None:
         if self.interaction.response.is_done():
-            await self.interaction.followup.send(*args, **kwargs)
-            return
-        await self.interaction.response.send_message(*args, **kwargs)
+            return await self.interaction.followup.send(*args, **kwargs)
+        try:
+            return await self.interaction.response.send_message(*args, **kwargs)
+        except discord.HTTPException:
+            return await self.interaction.followup.send(*args, **kwargs)
 
     async def get_beatmapset(self) -> Beatmapset:
         self.current_beatmapset = (
@@ -88,18 +104,24 @@ class BaseOsudleGame(ABC):
             f"Correct! {message.author.mention} guessed **{beatmapset.title}** by **{beatmapset.artist}**.",
         )
 
+    async def edit_latest_message(self, *args, **kwargs) -> None:
+        if self.latest_beatmap_message:
+            return await self.latest_beatmap_message.edit(*args, **kwargs)
+        await self.interaction.edit_original_response(*args, **kwargs)
+
+    async def remove_view(self) -> None:
+        if not self.current_view:
+            return
+
+        self.current_view.stop()
+        self.current_view = None
+        await self.edit_latest_message(view=None)
+
     async def do_next(self) -> None:
         if self.current_task:
-            self.current_task.cancel()
-            try:
-                await self.current_task
-            except asyncio.CancelledError:
-                pass
-            self.current_task = None
+            raise RuntimeError("Task already running.")
 
-            await self.send_response(
-                f"**{self.current_beatmapset.title}** by **{self.current_beatmapset.artist}** was the correct answer.",
-            )
+        await self.remove_view()
 
         beatmapset = await self.get_beatmapset()
         await self.send_message(beatmapset)
@@ -110,12 +132,21 @@ class BaseOsudleGame(ABC):
             )
             await self.current_task
             self.current_task = None
-        except asyncio.TimeoutError:
+        except asyncio.CancelledError:
             await self.send_response(
                 f"**{beatmapset.title}** by **{beatmapset.artist}** was the correct answer.",
             )
+        except asyncio.TimeoutError:
+            await self.send_response(
+                f"Nobody guessed the song. The correct answer was **{beatmapset.title}** by **{beatmapset.artist}**.",
+            )
             await self.stop_game()
             raise
+
+    def skip(self) -> None:
+        if self.current_task:
+            self.current_task.cancel()
+            self.current_task = None
 
     async def start_game(self, interaction: Interaction, mode: Gamemode) -> None:
         self.running = True
@@ -139,23 +170,35 @@ class BaseOsudleGame(ABC):
                 pass
             self.current_task = None
 
+        await self.remove_view()
+
         if not self.running or self.stop_event.is_set():
             return
 
         self.stop_event.set()
         self.running = False
+
         await self.send_response(
             f"Game has stopped.\nFinal scoreboard:\n{self.get_formatted_scoreboard()}",
         )
 
+    async def send_message(self, beatmapset: Beatmapset) -> None:
+        content = await self.get_message_content(beatmapset)
+        self.current_view = discord.ui.View()
+        self.current_view.add_item(OsudleSkipButton(self))
+        self.latest_beatmap_message = await self.send_response(
+            **content,
+            view=self.current_view,
+        )
+
     @abstractmethod
-    async def send_message(self, beatmapset: Beatmapset) -> None: ...
+    async def get_message_content(self, beatmapset: Beatmapset) -> dict[str, Any]: ...
 
 
 class OsudleSongGame(BaseOsudleGame):
     """osu! Song Preview Game"""
 
-    async def send_message(self, beatmapset: Beatmapset) -> None:
+    async def get_message_content(self, beatmapset: Beatmapset) -> dict:
         async with self.interaction.client.aiohttp_session.get(
             f"https:{beatmapset.preview_url}",
         ) as resp:
@@ -165,13 +208,15 @@ class OsudleSongGame(BaseOsudleGame):
                 return
             file_bytes = BytesIO(await resp.read())
             audio_file = File(file_bytes, filename="Preview.mp3")
-            await self.send_response(file=audio_file)
+            return {
+                "file": audio_file,
+            }
 
 
 class OsudleBackgroundGame(BaseOsudleGame):
     """osu! Background Game"""
 
-    async def send_message(self, beatmapset: Beatmapset) -> None:
+    async def get_message_content(self, beatmapset: Beatmapset) -> None:
         cover_url = beatmapset.covers.cover_2_x.replace(r"https:\/\/", "https://")
         async with self.interaction.client.aiohttp_session.get(cover_url) as resp:
             if resp.status != 200:
@@ -180,4 +225,6 @@ class OsudleBackgroundGame(BaseOsudleGame):
                 return
             file_bytes = BytesIO(await resp.read())
             image_file = File(file_bytes, filename="Background.jpg")
-            await self.send_response(file=image_file)
+            return {
+                "file": image_file,
+            }
--- src/cogs/osu.py
+++ src/cogs/osu.py
@@ -752,7 +752,7 @@ class OsudleCog(MetadataGroupCog, name="osudle"):
 
         self.running_games[channel.id].interaction = interaction
         try:
-            await self.running_games[channel.id].do_next()
+            self.running_games[channel.id].skip()
         except asyncio.TimeoutError:
             del self.running_games[channel.id]
 
